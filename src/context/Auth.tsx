6607a806fbc157dc51f33351b49c288b627ce6e8
Jun Jie <jjyong004@mymail.sim.edu.sg>
2026-02-16 00:25:01 +0800

    feat: auth context provider

---
+++ src/context/Auth.tsx
@@ -0,0 +1,89 @@
+import {
+  createContext,
+  useCallback,
+  useContext,
+  useMemo,
+  type ReactNode,
+} from 'react';
+import { useLocalStorageState } from '../hooks';
+
+type AuthContextData = {
+  currentUsername: string | undefined;
+  processLogin: (username: string, password: string) => boolean;
+  processSignup: (username: string, password: string) => boolean;
+  processLogout: () => void;
+};
+
+type User = {
+  username: string;
+  password: string;
+};
+
+const AuthContext = createContext<AuthContextData>({
+  currentUsername: undefined,
+  processLogin: () => false,
+  processSignup: () => false,
+  processLogout: () => false,
+});
+
+type Props = {
+  children: ReactNode;
+};
+
+export function AuthContextProvider({ children }: Props) {
+  const [users, setUsers] = useLocalStorageState<User[]>('users', []);
+  const [currentUsername, setCurrentUsername] = useLocalStorageState<
+    AuthContextData['currentUsername']
+  >('current', undefined);
+
+  const processLogin = useCallback(
+    (username: string, password: string): boolean => {
+      const existingUser = users.find(
+        user => user.username === username && user.password === password,
+      );
+
+      if (!existingUser) return false;
+
+      setCurrentUsername(username);
+      return true;
+    },
+    [users, setUsers],
+  );
+
+  const { processLogout, processSignup } = useMemo(() => {
+    const processSignup = (username: string, password: string): boolean => {
+      const userExists = users.some(user => user.username === username);
+      if (userExists) return false;
+
+      const newUser: User = { username, password };
+      setUsers(users => [...users, newUser]);
+      setCurrentUsername(username);
+
+      return true;
+    };
+
+    const processLogout = () => {
+      setCurrentUsername(undefined);
+    };
+
+    return { processLogout, processSignup };
+  }, [setUsers, setCurrentUsername]);
+
+  return (
+    <AuthContext.Provider
+      value={{ currentUsername, processLogin, processLogout, processSignup }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export function useAuth() {
+  const context = useContext(AuthContext);
+
+  if (!context) {
+    throw new Error('useAuth must be used within AuthContextProvider');
+  }
+
+  return context;
+}
